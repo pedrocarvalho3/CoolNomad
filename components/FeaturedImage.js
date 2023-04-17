18319299d41011a32f73a70e7ad956390585d5f2
pedrocarvalho3 <pdrpvp0310@gmail.com>
2023-04-17 12:21:16 -0300

    first commit

---
+++ components/FeaturedImage.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function FeaturedImage({ post }) {
+    let img = '';
+
+    const defaultFeaturedImage = "http://localhost:10004/wp-content/uploads/2022/12/travel_icy-polar_022K.jpg";
+    const defaultWidth = "300";
+    const defaultHeight = "200";
+
+    if(post.featuredImage) {
+        let size = post.featuredImage.node.mediaDetails.sizes[0];
+        img = {
+            src: size.sourceUrl,
+            width: size.width,
+            height: size.height
+        }
+    }
+    else {
+        img = {
+            src: defaultFeaturedImage,
+            width: defaultWidth,
+            height: defaultHeight
+        }
+    }
+
+    return (
+        <Link href={`/blog/${post.slug}`}>
+            <Image src={img.src} width={img.width} height={img.
+            height} alt={post.title} className="h-full
+            object-cover rounded-xl"/>
+        </Link>
+    )
+}
